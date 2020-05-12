3d4fb165a1df0ace0ba948e647c8c5630b8ce01b
Vaibhav Jha <vaibhavkumar2012@gmail.com>
2020-05-12 11:53:00 +0530

    Add files via upload

---
+++ Modules/vulnscanners/rapidscan.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+ORNG='\033[0;33m'
+NC='\033[0m'
+W='\033[1;37m'
+LP='\033[1;35m'
+YLW='\033[1;33m'
+LBBLUE='\e[104m'
+listing(){
+    echo -e "${YLW}"
+    cat files/pingtest.pass
+    echo -e "${NC}"
+}
+
+if [[ -d /opt/sifter/results/Rapidscan ]]; then
+    echo ""
+else
+    mkdir /opt/sifter/results/Rapidscan
+fi
+echo -e "${RED}"
+figlet -f mini "RapidscaN"
+echo -e "${NC}"
+echo -e "${YLW}Note: This module can take a while (30+ mins),"
+echo -e "and will be run in the background. Feel free to keep using the tool.${NC}"
+listing
+sleep 1
+echo "==================================================================================="
+xterm -e sudo rapidscan &
+echo -e "${W}When ready hit enter${NC}"
+cd /opt/sifter
+sifter -m
+
+
+##########################______________ czFsM250NzggX18gUmFiYjE3J3MgRGVu ______________##########################
---
+++ Modules/vulnscanners/yuki.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+RED='\033[0;31m'
+NC='\033[0m'
+ORNG='\033[0;33m'
+W='\033[1;37m'
+YLW='\033[1;33m'
+LBBLUE='\e[104m'
+
+echo -e "${ORNG}Yuki-Chan${NC}"
+echo -e "${ORNG}***********${NC}"
+echo -e "${YLW}"
+cat files/pingtest.pass
+echo -e "${NC}"
+echo -e "${W}Please copy and paste in your target site${NC}"
+echo "================================================================================================="
+cd /root/yuki
+sleep 1
+sudo ./startyuki
+echo "================================================================================================="
+cd /opt/sifter
+sifter -m
+
+
+##########################______________ czFsM250NzggX18gUmFiYjE3J3MgRGVu ______________##########################
